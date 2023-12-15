0d4c78b1004083e5e05f97fb80fc7e98ef114da3
gigiftt <gift.natcha@q-chang.com>
2023-12-15 19:17:14 +0700

    edit category response model

--- model/curriculum.go
+++ model/curriculum.go
@@ -1,6 +1,6 @@
 package model
 
-type CourseDetail struct {
+type CurriculumCourseDetail struct {
 	CourseNo          string   `json:"courseNo"`
 	RecommendSemester int      `json:"recommendSemester"`
 	RecommendYear     int      `json:"recommendYear"`
@@ -9,55 +9,24 @@ type CourseDetail struct {
 	Credits           int      `json:"credits"`
 }
 
-type GroupDetails struct {
-	RequiredCredits int            `json:"requiredCredits"`
-	GroupName       string         `json:"groupName"`
-	RequiredCourses []CourseDetail `json:"requiredCourses"`
-	ElectiveCourses []CourseDetail `json:"electiveCourses"`
+type CurriculumGroupDetails struct {
+	RequiredCredits int                      `json:"requiredCredits"`
+	GroupName       string                   `json:"groupName"`
+	RequiredCourses []CurriculumCourseDetail `json:"requiredCourses"`
+	ElectiveCourses []CurriculumCourseDetail `json:"electiveCourses"`
 }
 
 type Curriculum struct {
-	Program             string         `json:"curriculumProgram"`
-	Year                int            `json:"year"`
-	IsCOOPPlan          bool           `json:"isCOOPPlan"`
-	RequiredCredits     int            `json:"requiredCredits"`
-	FreeElectiveCredits int            `json:"freeElectiveCredits"`
-	CoreAndMajorGroups  []GroupDetails `json:"coreAndMajorGroups"`
-	GeGroups            []GroupDetails `json:"geGroups"`
+	Program             string                   `json:"curriculumProgram"`
+	Year                int                      `json:"year"`
+	IsCOOPPlan          bool                     `json:"isCOOPPlan"`
+	RequiredCredits     int                      `json:"requiredCredits"`
+	FreeElectiveCredits int                      `json:"freeElectiveCredits"`
+	CoreAndMajorGroups  []CurriculumGroupDetails `json:"coreAndMajorGroups"`
+	GeGroups            []CurriculumGroupDetails `json:"geGroups"`
 }
 
 type CurriculumModel struct {
 	Ok         bool       `json:"ok"`
 	Curriculum Curriculum `json:"curriculum"`
-}
-
-type CourseDetailResponse struct {
-	CourseNo   string `json:"courseNo"`
-	CourseName string `json:"courseName"`
-	GroupName  string `json:"groupName"`
-	IsPass     bool   `json:"isPass"`
-	X          int    `json:"x,omitempty"`
-	Y          int    `json:"y,omitempty"`
-}
-
-type CategoryDetail struct {
-	SummaryCredits  int                    `json:"summaryCredit"`
-	RequiredCredits int                    `json:"requiredCredits"`
-	CourseList      []CourseDetailResponse `json:"courseList"`
-}
-
-type CategoryResponse struct {
-	SummaryCredits  int            `json:"summaryCredit"`
-	RequiredCredits int            `json:"requiredCredits"`
-	CoreCategory    CategoryDetail `json:"coreCategory"`
-	MajorCategory   CategoryDetail `json:"majorCategory"`
-	GECategory      CategoryDetail `json:"geCategory"`
-	FreeCategory    CategoryDetail `json:"freeCategory"`
-}
-
-type Credits struct {
-	CoreCredits  int
-	MajorCredits int
-	GeCredits    int
-	FreeCredits  int
 }
---
+++ model/resoponse.go
@@ -0,0 +1,66 @@
+package model
+
+type GetCourseDetail struct {
+	Ok           bool           `json:"ok"`
+	CourseDetail []CourseDetail `json:"courseDetails"`
+}
+type CourseDetail struct {
+	CourseNo              string                       `json:"courseNo"`
+	UpdatedYear           int                          `json:"updatedYear"`
+	UpdatedSemester       int                          `json:"updatedSemester"`
+	CourseNameEN          string                       `json:"courseNameEN"`
+	CourseNameTH          string                       `json:"courseNameTH"`
+	CurCodeEN             string                       `json:"curCodeEN"`
+	CurCodeTH             string                       `json:"curCodeTH"`
+	DetailEN              string                       `json:"detailEN"`
+	DetailTH              string                       `json:"detailTH"`
+	Credits               CreditDetail                 `json:"credits"`
+	SelectedTopicSubjects []SelectedTopicSubjectDetail `json:"selectedTopicSubjects"`
+}
+
+type CreditDetail struct {
+	Credits   int `json:"credits"`
+	Lecture   int `json:"lecture"`
+	Practice  int `json:"practice"`
+	SelfStudy int `json:"selfStudy"`
+}
+
+type SelectedTopicSubjectDetail struct {
+	SubjectId    string `json:"subjectId"`
+	SubjectTitle string `json:"subjectTitle"`
+	IsActive     bool   `json:"isActive"`
+}
+
+type CourseDetailResponse struct {
+	CourseNo   string `json:"courseNo"`
+	CourseName string `json:"courseName"`
+	GroupName  string `json:"groupName"`
+	IsPass     bool   `json:"isPass"`
+	X          int    `json:"x,omitempty"`
+	Y          int    `json:"y,omitempty"`
+}
+
+type CategoryResponse struct {
+	SummaryCredits  int              `json:"summaryCredit"`
+	RequiredCredits int              `json:"requiredCredits"`
+	CoreCategory    []CategoryDetail `json:"coreCategory"`
+	MajorCategory   []CategoryDetail `json:"majorCategory"`
+	GECategory      []CategoryDetail `json:"geCategory"`
+	FreeCategory    []CategoryDetail `json:"freeCategory"`
+}
+
+type CategoryDetail struct {
+	GroupName           string                 `json:"groupName"`
+	RequiredCreditsNeed int                    `json:"requiredCreditsNeed"`
+	RequiredCreditsGet  int                    `json:"requiredCreditsGet"`
+	ElectiveCreditsNeed int                    `json:"electiveCreditsNeed"`
+	ElectiveCreditsGet  int                    `json:"electiveCreditsGet"`
+	RequiredCourseList  []CourseDetailResponse `json:"requiredCourseList"`
+	ElectiveCourseList  []CourseDetailResponse `json:"electiveCourseList"`
+}
+type Credits struct {
+	CoreCredits  int
+	MajorCredits int
+	GeCredits    int
+	FreeCredits  int
+}
