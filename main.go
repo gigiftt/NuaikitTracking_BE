e559f1ae6f08edb04cb3822786f9eb850faa1e69
gigiftt <gift.natcha@q-chang.com>
2024-02-20 15:34:35 +0700

    add course preList

--- main.go
+++ main.go
@@ -1309,6 +1309,7 @@ func putInTemplate(templateArr [][]string, x int, corequisiteList []string, noPr
 			for _, c := range prerequisites {
 
 				course := c.String()
+				prerequisitesList = append(prerequisitesList, course)
 
 				havePreReq := slices.Contains[[]string](havePreList, course)
 
